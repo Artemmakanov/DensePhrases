a8c4252b81ec495d76a4bac1fb85f963843c6d1b
Artemmakanov <artem.makanov@mail.ru>
2024-02-26 18:20:22 +0000

    Адаптировал модуль Олеси в ядро

--- src/dataset_qa.py
+++ src/dataset_qa.py
@@ -2,12 +2,60 @@ import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import Dataset
-
+from tqdm import tqdm
 
 class Dataset_QA(Dataset):
-    def __init__(self, df, tokenizer, max_length):
-        self.context = df.context.values
-        self.question = df.question.values
+    def __init__(
+        self,
+        tokenizer,
+        max_length,
+        contexts,
+        questions,
+        answers,
+        size
+    ):
+        self.tokenizer = tokenizer
+        self.ids = []
+        self.contexts = []
+        self.questions = []
+        self.answers = []
+        self.spans_input_ids = []
+        id = 0
+        
+        if size:
+            treshold = size
+            self.size = size
+        else:
+            treshold = len(contexts)
+        for i in tqdm(range(treshold)):
+            context = contexts[i]
+            answer = answers[i]
+
+            answer_tokenized = self.tokenizer(answer)['input_ids'][1:-1]
+            context_tokenized = self.tokenizer(context)['input_ids'][1:-1]
+                    
+            for context_start_token in range(len(context_tokenized) - len(answer_tokenized)):
+                
+                if answer_tokenized == context_tokenized[context_start_token:context_start_token+len(answer_tokenized)]:
+                    break
+                context_start_token += 1
+
+            
+            context_end_token = context_start_token + len(answer_tokenized)
+
+            if context_start_token < 512 and context_end_token < 512: 
+                self.spans_input_ids.append({
+                    'start':context_start_token,
+                    'end': context_end_token
+                })
+                
+                self.contexts.append(contexts[i])
+                self.questions.append(questions[i])
+                self.answers.append(answer)
+
+                self.ids.append(id)
+                id += 1
+           
         self.tokenizer = tokenizer
         self.max_length = max_length
     
@@ -29,8 +77,10 @@ class Dataset_QA(Dataset):
         )
 
         return {'context_ids': inputs_context,
-                'question_ids': inputs_question
+                'question_ids': inputs_question,
+                'span_input_start_ids': self.spans_input_ids[idx]['start'],
+                'span_input_end_ids': self.spans_input_ids[idx]['ends']
                }
-
+        
     def __len__(self):
         return len(self.context)
--- src/model_phase.py
+++ src/model_phase.py
@@ -9,7 +9,7 @@ import torch
 from tqdm import tqdm
 
 class PhraseModel(torch.nn.Module):
-    def __init__(self, hidden_dim, ds, model_checkpoint, device):
+    def __init__(self, hidden_dim, dataset, model_checkpoint, device):
         super(PhraseModel, self).__init__()
 
         self.device = device
@@ -20,26 +20,26 @@ class PhraseModel(torch.nn.Module):
         self.model = AutoModel.from_pretrained(model_checkpoint).to(self.device)
         self.model_start = AutoModel.from_pretrained(model_checkpoint).to(self.device)
         self.model_end = AutoModel.from_pretrained(model_checkpoint).to(self.device)
-        self.ds = ds
+        self.dataset = dataset
         self.softmax = torch.nn.Softmax()
         
 
-    def forward(self, ids: List[int]):
-        context_ids = self.tokenizer(self.ds.get_contexts(ids), 
-                                     truncation=True, max_length=512, 
-                                     return_tensors="pt", padding=True).to(self.device)
-        question_ids = self.tokenizer(self.ds.get_questions(ids), 
-                                      truncation=True, max_length=512, 
-                                      return_tensors="pt", padding=True).to(self.device)
+    def forward(
+        self,
+        context_ids,
+        question_ids,
+        start_token_indices,
+        end_token_indices
+    ):
+
         last_hidden_state = self.model(**context_ids).last_hidden_state[1:-1]
         
         N, context_num_tokens, _ = last_hidden_state.shape
         q_start = self.model_start(**question_ids).last_hidden_state[:, 0]
         q_end = self.model_end(**question_ids).last_hidden_state[:, 0]
-        start_token_indices = self.ds.get_spans_input_ids(ids, pos='start')
-        end_token_indices = self.ds.get_spans_input_ids(ids, pos='end')
+        # start_token_indices = self.dataset.get_spans_input_ids(ids, pos='start')
+        # end_token_indices = self.dataset.get_spans_input_ids(ids, pos='end')
         loss = torch.Tensor([0.]).to(self.device)
-        eps = torch.Tensor([1e-5]).to(self.device)
         # print(f"context_num_tokens {context_num_tokens}")
         # print(f"last_hidden_state.shape {last_hidden_state.shape}")
         # print(f"q_start.shape {q_start.shape}")
@@ -50,11 +50,11 @@ class PhraseModel(torch.nn.Module):
             
             z_start = torch.matmul(last_hidden_state[n], q_start[n].T).reshape(context_num_tokens)
             P_start = self.softmax(z_start)
-            loss_start = torch.log(P_start[start_token_indices[n]] + eps)
+            loss_start = torch.log(P_start[start_token_indices[n]])
             
             z_end = torch.matmul(last_hidden_state[n], q_end[n].T).reshape(context_num_tokens)
             P_end = self.softmax(z_end)
-            loss_end = torch.log(P_end[end_token_indices[n]] + eps)
+            loss_end = torch.log(P_end[end_token_indices[n]])
             
             loss += (loss_start + loss_end) / 2
 
